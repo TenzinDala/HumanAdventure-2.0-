5775c52ebad1e6d25ddca4c057b3333af70a3fee
TenzinDala <tenzindala2000@gmail.com>
2023-05-13 09:52:27 -0400

    Added Class

---
+++ HumanAdventure/HumanAdventure/Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanNamespace
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Human(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        //Consumed Either Poison or correct Food. If and else for correct food or not
+        public void Eat(string food)
+        {
+
+        }
+        //Exercise if and else for time. If too long then you die.
+        public void Exercise(string Time) 
+        { 
+        
+        }
+    }
+}
--- HumanAdventure/HumanAdventure/Program.cs
+++ HumanAdventure/HumanAdventure/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace HumanAdventure
+﻿//Programmer - Tenzin Dala
+//StudentID - 134244219
+
+
+using HumanNamespace;
+
+namespace HumanAdventure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            var Tenzin = new Human("Tenzin", 22, "Male");
+            Console.WriteLine($"Human's name is {Tenzin.Name}. He is {Tenzin.Gender} with the age of {Tenzin.Age}");
         }
     }
 }
